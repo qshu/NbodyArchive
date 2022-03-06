092a64e1a4ac3838afba528b61d47917a6247cdc
bilibilihuangyifan <1730186686@qq.com>
2022-03-06 06:04:46 +0100

    init

---
+++ noauto.bat
@@ -0,0 +1,4 @@
+mpif77 -g -fbounds-check -fbacktrace -fno-automatic -O3 -fPIC -mcmodel=large -Iinclude -D GPU -D PARALLEL -D PUREMPI -c -o build/intgrt.o src/Main/intgrt.F
+mpif77 -g -fbounds-check -fbacktrace -fno-automatic -O3 -fPIC -mcmodel=large -Iinclude -D GPU -D PARALLEL -D PUREMPI -c -o build/intgrt.o src/Main/intgrt.F
+mpif77 -g -fbounds-check -fbacktrace -fno-automatic -O3 -fPIC -mcmodel=large -Iinclude -D GPU -D PARALLEL -D PUREMPI -c -o build/intgrt.o src/Main/intgrt.F
+mpif77 -g -fbounds-check -fbacktrace -fno-automatic -O3 -fPIC -mcmodel=large -Iinclude -D GPU -D PARALLEL -D PUREMPI -c -o build/intgrt.o src/Main/intgrt.F
